865b76f31a2d9e14a439c03c4c669f8891c25677
suraj1414 <78675641+suraj1414@users.noreply.github.com>
2021-02-14 09:35:57 +0530

    Update Demo2.java

--- automation/src/qsp/Demo2.java
+++ automation/src/qsp/Demo2.java
@@ -5,6 +5,7 @@ public class Demo2 {
 	
 	public static void main(String[] args) {
 		System.out.println("Demo2");
+		System.out.println("Hi this is the new modification done in the github");
 	}
 
 }
